49377510385a45c569b94e7da707e45558bfa723
James Muñoz <jamb1993@gmail.com>
2023-02-10 18:23:12 -0500

    Estructura del proyecto creada

---
+++ src/test/java/com/reto/atencionMedica/AtencionMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.atencionMedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtencionMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
